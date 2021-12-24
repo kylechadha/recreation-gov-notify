3b89367e0be3280caee820c513f96255d92c614e
Kyle Chadha <kyle.chadha@gmail.com>
2021-12-23 19:04:27 -0800

    Add date param parsing

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kylechadha/recreation-gov-notify
+
+go 1.16
+
+require (
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/inconshreveable/log15 v0.0.0-20201112154412-8562bdadbbac
+	github.com/mattn/go-colorable v0.1.12 // indirect
+)
--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
+
+	"github.com/inconshreveable/log15"
 )
 
 type Campsites struct {
@@ -16,79 +19,102 @@ type Campsites struct {
 }
 
 type Campsite struct {
-	Availabilities      map[string]Availability `json:"availabilities"`
-	CampsiteID          string                  `json:"campsite_id"`
-	CampsiteReserveType string                  `json:"campsite_reserve_type"`
-	CampsiteType        string                  `json:"campsite_type"`
-	Loop                string                  `json:"loop"`
-	MaxNumPeople        int                     `json:"max_num_people"`
-	MinNumPeople        int                     `json:"min_num_people"`
-	Site                string                  `json:"site"`
-	TypeOfUse           string                  `json:"type_of_use"`
+	Availabilities      map[string]string `json:"availabilities"`
+	CampsiteID          string            `json:"campsite_id"`
+	CampsiteReserveType string            `json:"campsite_reserve_type"`
+	CampsiteType        string            `json:"campsite_type"`
+	Loop                string            `json:"loop"`
+	MaxNumPeople        int               `json:"max_num_people"`
+	MinNumPeople        int               `json:"min_num_people"`
+	Site                string            `json:"site"`
+	TypeOfUse           string            `json:"type_of_use"`
 }
 
-type Availability string
-
-const (
-	Unknown       Availability = "Unknown"
-	Available     Availability = "Available"
-	NotAvailable  Availability = "Not Available"
-	Reserved      Availability = "Reserved"
-	NotReservable Availability = "Not Reservable"
-)
-
 func main() {
+	l := log15.New()
 	client := http.Client{}
 
 	// ** HERE
-	// start building params in part
 	// then visual display out
 	// then connect to text/email
-	// then make CLI app
+	// then convert to CLI app
 
+	debug := true
 	campground := 231959
+	startDate := "2021-12-27"
+	endDate := "2022-01-03"
+
+	l.Info(fmt.Sprintf("Searching recreation.gov for availability from %s to %s", startDate, endDate))
+
+	if !debug {
+		l.SetHandler(log15.LvlFilterHandler(log15.LvlInfo, log15.StdoutHandler))
+	}
+
+	st, err := time.Parse("2006-01-02", startDate)
+	if err != nil {
+		log.Fatal(err)
+	}
+	et, err := time.Parse("2006-01-02", endDate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	startPeriod := fmt.Sprintf("%d-%02d-01", st.Year(), st.Month())
+	endPeriod := fmt.Sprintf("%d-%02d-01", et.Year(), et.Month())
+
+	var months []string
+	months = append(months, startPeriod)
+
+	if startPeriod != endPeriod {
+		for st.Before(et) {
+			st = st.AddDate(0, 1, 0)
+			months = append(months, fmt.Sprintf("%d-%02d-01", st.Year(), st.Month()))
+		}
+	}
+
+	available := make(map[string]map[string]bool)
 	baseURL := fmt.Sprintf("https://www.recreation.gov/api/camps/availability/campground/%d/month", campground)
-	startDate := "2021-12-01"
+	for _, m := range months {
+		l.Debug(fmt.Sprintf("Requesting data for %s", strings.TrimSuffix(m, "-01")))
 
-	req, err := http.NewRequest("GET", baseURL, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	q := url.Values{}
-	q.Add("start_date", startDate+"T00:00:00.000Z")
-	req.URL.RawQuery = q.Encode()
-
-	// Need to spoof the user agent or CloudFront blocks us.
-	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		bytes, err := ioutil.ReadAll(resp.Body)
+		req, err := http.NewRequest("GET", baseURL, nil)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Fatal(resp.StatusCode, ": ", string(bytes))
-	}
 
-	var cs Campsites
-	err = json.NewDecoder(resp.Body).Decode(&cs)
-	if err != nil {
-		log.Fatal(err)
-	}
+		q := url.Values{}
+		q.Add("start_date", m+"T00:00:00.000Z")
+		req.URL.RawQuery = q.Encode()
 
-	var dates []string
-	for _, c := range cs.Campsites {
-		for date, a := range c.Availabilities {
-			if a == Available {
-				dates = append(dates, strings.TrimSuffix(date, "T00:00:00Z"))
+		// Need to spoof the user agent or CloudFront blocks us.
+		req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			bytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Fatal(resp.StatusCode, ": ", string(bytes))
+		}
+
+		var cs Campsites
+		err = json.NewDecoder(resp.Body).Decode(&cs)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, c := range cs.Campsites {
+			for date, a := range c.Availabilities {
+				if a == "Available" {
+
+				}
 			}
 		}
 	}
-	fmt.Println(dates)
 }
