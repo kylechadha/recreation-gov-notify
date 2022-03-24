74ae20190efdc09cb2b980b231f182488df1aad7
Kyle Chadha <kyle.chadha@gmail.com>
2022-03-24 14:58:43 -0700

    Clean up date parsing; Fix bug in availablity string comparison

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"strings"
+	"os"
 	"time"
 
 	"github.com/inconshreveable/log15"
@@ -30,52 +30,74 @@ type Campsite struct {
 	TypeOfUse           string            `json:"type_of_use"`
 }
 
+// ** HERE
+// then connect to text/email
+// then convert to CLI app
+// - Split out front end vs back end here
+// - How do we programmatically determine campground ID? Is there another API we can hit to search?
+// then create web frontend
+// productionize w/ cloud functions?
+// - Do this when you built the front end, either we have one backend app that spins up goroutines to poll
+// Or spins up a cloud function
+
 func main() {
+	exitCode := 0
+	defer func() {
+		os.Exit(exitCode)
+	}()
+
 	l := log15.New()
 	client := http.Client{}
 
-	// ** HERE
-	// then visual display out
-	// then connect to text/email
-	// then convert to CLI app
-
 	debug := true
-	campground := 231959
-	startDate := "2021-12-27"
-	endDate := "2022-01-03"
-
-	l.Info(fmt.Sprintf("Searching recreation.gov for availability from %s to %s", startDate, endDate))
-
 	if !debug {
 		l.SetHandler(log15.LvlFilterHandler(log15.LvlInfo, log15.StdoutHandler))
 	}
 
-	st, err := time.Parse("2006-01-02", startDate)
+	campground := 231969
+	checkInDate := "2022-06-01"
+	checkOutDate := "2022-06-02"
+
+	l.Info("Searching recreation.gov...", "campground", campground, "checkIn", checkInDate, "checkOut", checkOutDate)
+
+	st, err := time.Parse("2006-01-02", checkInDate)
 	if err != nil {
-		log.Fatal(err)
+		l.Error("Invalid check in date", "err", err)
+		exitCode = 1
+		return
 	}
-	et, err := time.Parse("2006-01-02", endDate)
+	etRaw, err := time.Parse("2006-01-02", checkOutDate)
 	if err != nil {
-		log.Fatal(err)
+		l.Error("Invalid check out date", "err", err)
+		exitCode = 1
+		return
+	}
+	et := etRaw.AddDate(0, 0, -1) // checkOutDate does not need to be available
+
+	if st.After(et) {
+		l.Error("Start date is after end date")
+		exitCode = 1
+		return
 	}
 
-	startPeriod := fmt.Sprintf("%d-%02d-01", st.Year(), st.Month())
-	endPeriod := fmt.Sprintf("%d-%02d-01", et.Year(), et.Month())
+	curPeriod := fmt.Sprintf("%d-%02d", st.Year(), st.Month())
+	endPeriod := fmt.Sprintf("%d-%02d", et.Year(), et.Month())
 
 	var months []string
-	months = append(months, startPeriod)
+	months = append(months, curPeriod)
 
-	if startPeriod != endPeriod {
-		for st.Before(et) {
-			st = st.AddDate(0, 1, 0)
-			months = append(months, fmt.Sprintf("%d-%02d-01", st.Year(), st.Month()))
-		}
+	initial := st
+	for curPeriod != endPeriod {
+		st = st.AddDate(0, 1, 0)
+		curPeriod = fmt.Sprintf("%d-%02d", st.Year(), st.Month())
+		months = append(months, curPeriod)
 	}
+	st = initial
 
 	available := make(map[string]map[string]bool)
 	baseURL := fmt.Sprintf("https://www.recreation.gov/api/camps/availability/campground/%d/month", campground)
 	for _, m := range months {
-		l.Debug(fmt.Sprintf("Requesting data for %s", strings.TrimSuffix(m, "-01")))
+		l.Debug("Requesting data", "month", m)
 
 		req, err := http.NewRequest("GET", baseURL, nil)
 		if err != nil {
@@ -83,7 +105,7 @@ func main() {
 		}
 
 		q := url.Values{}
-		q.Add("start_date", m+"T00:00:00.000Z")
+		q.Add("start_date", m+"-01T00:00:00.000Z")
 		req.URL.RawQuery = q.Encode()
 
 		// Need to spoof the user agent or CloudFront blocks us.
@@ -112,9 +134,35 @@ func main() {
 		for _, c := range cs.Campsites {
 			for date, a := range c.Availabilities {
 				if a == "Available" {
+					if available[c.Site] == nil {
+						available[c.Site] = make(map[string]bool)
+					}
 
+					available[c.Site][date] = true
 				}
 			}
 		}
+	}
+
+	var results []string
+Outer:
+	for site, dates := range available {
+		st = initial
+		for !st.After(et) {
+			date := fmt.Sprintf("%sT00:00:00Z", st.Format("2006-01-02"))
+			if dates[date] {
+				l.Debug(fmt.Sprintf("Site %s available for %s", site, st.Format("2006-01-02")))
+				st = st.AddDate(0, 0, 1)
+			} else {
+				continue Outer
+			}
+		}
+
+		l.Info(fmt.Sprintf("Site %s is available", site))
+		results = append(results, site)
+	}
+
+	if len(results) == 0 {
+		l.Info("Sorry, no available campsites were found for the full date range")
 	}
 }
